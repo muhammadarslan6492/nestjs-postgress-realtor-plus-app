84a393ef5e6b16dbf0535026b1d0d58bf6f8c763
muhammad arslan <muhammadarslan6492@gmail.com>
2023-06-02 13:09:30 -0700

    feat:set all endpoints for home controller

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Delete,
+  Post,
+  Body,
+  InternalServerErrorException,
+  Param,
+  ParseEnumPipe,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 @Controller('home')
-export class HomeController {}
+export class HomeController {
+  @Get()
+  getHomes() {
+    return [];
+  }
+
+  @Get(':id')
+  getHome() {
+    return {};
+  }
+
+  @Post()
+  createHome() {
+    return {};
+  }
+
+  @Put(':id')
+  updateHome() {
+    return {};
+  }
+
+  @Delete(':id')
+  deleteHome() {
+    return {};
+  }
+}
